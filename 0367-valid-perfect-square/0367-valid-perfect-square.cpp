b12b2bb5334c22e78b611a8b09b0bbb2924ccf4f
Ritesh2028 <138420477+Ritesh2028@users.noreply.github.com>
2025-11-19 00:44:36 +0530

    Time: 0 ms (100%), Space: 7.6 MB (99.56%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.cpp
+++ 0367-valid-perfect-square/0367-valid-perfect-square.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool isPerfectSquare(int num) {
         if (num==1) return true;
-        for(long  i =1;i<num;i++){
+        for(long  i =1;i<56000;i++){
             long x=i*i;
             if(x==num){
                 return true;
